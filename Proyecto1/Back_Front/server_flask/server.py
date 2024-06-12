4f25f2aaa37cdf380df23ff80b5461fa99d704bf
C-FernandoSazo <cesarfernando.sazo@gmail.com>
2024-06-12 13:04:52 -0600

    Cambios

--- Proyecto1/Back_Front/server_flask/server.py
+++ Proyecto1/Back_Front/server_flask/server.py
@@ -10,14 +10,14 @@ app = Flask(__name__)
 CORS(app)
 socketio = SocketIO(app, cors_allowed_origins="*")
 
-lcd = LCD(2,0x27) 
+lcd = LCD(2, 0x27)
 
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BOARD)  # Asegurando que el modo de numeración de pines se establece al inicio
 
-#Sensor Fotorresistencia
+# Sensor Fotorresistencia
 sensor_exterior = 16
 
-#Sensor Interior
+# Sensor Interior
 BCD0 = 8
 BCD1 = 10
 BCD2 = 11
@@ -25,11 +25,11 @@ BCD3 = 13
 SensorEntrada = 19
 SensorSalida = 21
 
-#Sensor Digital
+# Sensor Digital
 sensor1 = 7
 sensor2 = 15
 
-#Leds
+# Leds
 Exterior = 38 
 Area_Recepcion = 36
 Area_Conferencia = 40
@@ -39,17 +39,17 @@ Cafeteria = 24
 Bano = 26
 Area_Transporte = 22
 
-#Sensor Perimetral
+# Sensor Perimetral
 buzzer_pin = 29
 ldr_pin = 31
 
-#Motor Banda
+# Motor Banda
 pinA = 16
 pinB = 18
 pinEnable = 12  
 pwm1 = None
 
-#Motor Garage
+# Motor Garage
 pin1 = 23
 pin2 = 32
 pinEna = 33
@@ -91,14 +91,13 @@ pin_mapping = {
     "Area_Transporte": 22
 }
 
-#Hilos
+# Hilos
 
 # Sensor de fotoresistencia exterior
 def sensorExterior():
     global pause_thread
     try:
         while True:
-            print(pause_thread)
             if not pause_thread:
                 # Lee el estado del sensor de luz
                 if GPIO.input(sensor_exterior):
@@ -157,7 +156,7 @@ def sensorDigital():
         GPIO.cleanup()
 
 
-#Manejo de solicitudes
+# Manejo de solicitudes
 
 @app.route('/')
 def home():
@@ -224,7 +223,6 @@ def handle_gate():
         
 @app.route('/api/conveyor', methods=['POST'])
 def toggle_gate():
-    
     if not state["isConveyorMoving"]:
         motor_adelante_banda(75)
         state["isConveyorMoving"] = True
@@ -235,17 +233,20 @@ def toggle_gate():
     # Devolver el estado actual de la puerta y si fue abierta o cerrada
     return jsonify({"success": True, "isGateOpen": state["isConveyorMoving"]})
 
-#Sensor Perimetral
-def toggle_alarm():
+# Sensor Perimetral
+def alarmaPerimetral():
     try:
         while True:
             # Lee el estado del sensor de luz
+            print("ciclo")
             if GPIO.input(ldr_pin):
-                buzz(1000,5)
+                print("Se detecto movimientos")
+                sleep(5)
                 state["isAlarmActive"] = True
             else:
+                print("No movimiento ")
                 state["isAlarmActive"] = False
-            sleep(1) 
+            sleep(0.5)
     except KeyboardInterrupt:
         GPIO.cleanup()
         print("Programa interrumpido y GPIO limpio")
@@ -255,52 +256,52 @@ def buzz(pitch, duration):
     delay = period / 2
     cycles = int(duration * pitch)
     for i in range(cycles):
-        GPIO.output(buzzer_pin, True)
+        GPIO.output(buzzer_pin, GPIO.HIGH)
         sleep(delay)
-        GPIO.output(buzzer_pin, False)
+        GPIO.output(buzzer_pin, GPIO.LOW)
         sleep(delay)
 
 
-#Motor Banda
+# Motor Banda
 
 def motor_adelante_banda(velocidad):
     global pwm1, pinA, pinB
-    lcd.message("Abriendo...",1)
-    pwm1.ChangeDutyCycle(velocidad)  
+    lcd.message("Abriendo...", 1)
+    pwm1.ChangeDutyCycle(velocidad)
     GPIO.output(pinA, GPIO.HIGH)
     GPIO.output(pinB, GPIO.LOW)
 
 def detener_banda():
     global pwm1, pinA, pinB
-    lcd.message("Esperando...",1)
+    lcd.message("Esperando...", 1)
     pwm1.ChangeDutyCycle(0)  # Motor apagado
     GPIO.output(pinA, GPIO.LOW)
     GPIO.output(pinB, GPIO.LOW)
 
 
-#Motor Garage
+# Motor Garage
 def motor_adelante_porton(velocidad):
     global pwm2, pin1, pin2
-    lcd.message("Abriendo...",1)
-    pwm2.ChangeDutyCycle(velocidad)  
+    lcd.message("Abriendo...", 1)
+    pwm2.ChangeDutyCycle(velocidad)
     GPIO.output(pin1, GPIO.HIGH)
     GPIO.output(pin2, GPIO.LOW)
 
 def motor_atras_porton(velocidad):
     global pwm2, pin1, pin2
-    lcd.message("Cerrando...",1)
+    lcd.message("Cerrando...", 1)
     pwm2.ChangeDutyCycle(velocidad)
     GPIO.output(pin1, GPIO.LOW)
     GPIO.output(pin2, GPIO.HIGH)
 
 def detener_porton():
     global pwm2, pin1, pin2
-    lcd.message("Esperando...",1)
+    lcd.message("Esperando...", 1)
     pwm2.ChangeDutyCycle(0)  # Motor apagado
     GPIO.output(pin1, GPIO.LOW)
     GPIO.output(pin2, GPIO.LOW)
 
-#Sensor Interior Display
+# Sensor Interior Display
 # Variables
 numerosBinario = [
     [0, 0, 0, 0],
@@ -390,7 +391,7 @@ def increment_people_count():
         
 def setup():
     global pwm1, pwm2  # Agregar esta línea para indicar que pwm es global
-    #Pines de Leds
+    # Pines de Leds
     GPIO.setup(Exterior, GPIO.OUT)
     GPIO.setup(Area_Recepcion, GPIO.OUT)
     GPIO.setup(Area_Conferencia, GPIO.OUT)
@@ -399,34 +400,8 @@ def setup():
     GPIO.setup(Cafeteria, GPIO.OUT)
     GPIO.setup(Bano, GPIO.OUT)
     GPIO.setup(Area_Transporte, GPIO.OUT)
-    #Pin Sensor Exterior
+    # Pin Sensor Exterior
     GPIO.setup(sensor_exterior, GPIO.IN)
-    #Pines Sensor Perimetral
-    GPIO.setup(buzzer_pin, GPIO.OUT)
-    GPIO.setup(ldr_pin, GPIO.IN)
-    #Pines Sensor Digital
-    GPIO.setup(sensor1, GPIO.IN)
-    GPIO.setup(sensor2, GPIO.IN)
-    #Pines Sensor Display
-    GPIO.setup(BCD0, GPIO.OUT)
-    GPIO.setup(BCD1, GPIO.OUT)
-    GPIO.setup(BCD2, GPIO.OUT)
-    GPIO.setup(BCD3, GPIO.OUT)
-    GPIO.setup(SensorEntrada, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    GPIO.setup(SensorSalida, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    #Pines Motor Banda
-    GPIO.setup(pinA, GPIO.OUT)
-    GPIO.setup(pinB, GPIO.OUT)
-    GPIO.setup(pinEnable, GPIO.OUT)
-    #Pines Motor Porton
-    GPIO.setup(pin1, GPIO.OUT)
-    GPIO.setup(pin2, GPIO.OUT)
-    GPIO.setup(pinEna, GPIO.OUT)
-    pwm1 = GPIO.PWM(pinEnable, 1000)  # Frecuencia de PWM de 1000 Hz
-    pwm1.start(0)
-    pwm2 = GPIO.PWM(pinEna, 1000)  # Frecuencia de PWM de 1000 Hz
-    pwm2.start(0)
-
 
 
 if __name__ == '__main__':
@@ -436,12 +411,10 @@ if __name__ == '__main__':
         lcd.message("<VACAS_JUN_24>", 2)
         sleep(5)
         lcd.clear()
-        hilo = threading.Thread(target=sensorInterior)
+        hilo = threading.Thread(target=sensorExterior)
         hilo.start()
         socketio.run(app, debug=True)
     except KeyboardInterrupt:
         GPIO.cleanup()
     finally:
         GPIO.cleanup()
-
-
